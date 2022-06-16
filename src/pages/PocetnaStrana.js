26438d6dd685c9387a21c5f071d3206de835a7a7
lucadenuca <luka.filipovic.fon@gmail.com>
2022-06-16 08:14:20 +0200

    Dobijanje svih gradilista iz baze podataka koriscenjem axios zahteva i useEffect Hooksa

--- src/pages/PocetnaStrana.js
+++ src/pages/PocetnaStrana.js
@@ -1,7 +1,21 @@
 import SvaGradilista from "../components/SvaGradilista";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 function PocetnaStrana() {
+
+    const [gradilista, setGradilista] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/gradiliste').then(response => {
+
+            setGradilista(response.data.data);
+
+        });
+
+    }, []);
+
 
     return (
         <div>
